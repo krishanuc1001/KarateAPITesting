5132da9463ce5c04e0dce4bbdcd966f3b4526668
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2024-05-19 00:43:24 +0530

    Refactor RunnerTest class to use karate-junit5 for running features

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -1,11 +1,22 @@
 package runner;
 
-import com.intuit.karate.Runner;
+import com.intuit.karate.junit5.Karate;
 
 public class RunnerTest {
 
-  public static void main(String[] args) {
-    Runner.path("src/test/java/features").parallel(2);
+  /**
+   * This method is used to run the Karate tests using Karate-core dependency.
+   */
+  //  public static void main(String[] args) {
+  //    Runner.path("src/test/java/features").parallel(2);
+  //  }
+
+  /**
+   * This method is used to run the Karate tests using Karate-junit5 dependency.
+   */
+  @Karate.Test
+  Karate testRunner() {
+    return Karate.run("classpath:features");
   }
 
 }
